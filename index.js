26a812a0e5e649ed07aba7bf647e0298c868f330
Sumankdatta <sumankdatta18@gmail.com>
2022-11-11 15:39:44 +0600

    review api added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const app =express();
-const port=process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 app.use(cors());
@@ -14,17 +14,55 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.f1hhq8d.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        const serviceCollection=client.db('travelguide').collection('services');
+        const reviwesCollection=client.db('travelguide').collection('reviews');
 
 
-app.get('/',(req,res)=>{
+        app.get('/services',async(req,res)=>{
+            const query={}
+            const cursor=serviceCollection.find(query);
+            const services =await cursor.limit(3).toArray();
+            res.send(services)
+        })
+        app.get('/servicesall',async(req,res)=>{
+            const query={}
+            const cursor=serviceCollection.find(query);
+            const services =await cursor.toArray();
+            res.send(services)
+        })
+       
+
+        app.get('/services/:id',async(req,res)=>{
+            const id=req.params.id;
+            const query={_id: ObjectId(id)}
+            const service=await serviceCollection.findOne(query);
+            res.send(service)
+        })
+
+        // reviews api
+
+        app.post('/reviews',async(req,res)=>{
+            const review=req.body;
+            const result = await reviwesCollection.insertOne(review);
+            res.send(result)
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
+
+
+app.get('/', (req, res) => {
     res.send('travel server is running')
 })
 
-app.listen(port,() =>{
-console.log(`travel server is running on ${port}`)
+app.listen(port, () => {
+    console.log(`travel server is running on ${port}`)
 })
